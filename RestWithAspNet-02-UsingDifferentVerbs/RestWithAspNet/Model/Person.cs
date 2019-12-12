c5b2971f3ff249e0ffa06edeefe28242863b3e1c
jorgestrobel <troba76@gmail.com>
2019-12-12 15:29:21 -0300

    Defining methods in PersonService

--- RestWithAspNet-02-UsingDifferentVerbs/RestWithAspNet/Model/Person.cs
+++ RestWithAspNet-02-UsingDifferentVerbs/RestWithAspNet/Model/Person.cs
@@ -7,7 +7,7 @@ namespace RestWithAspNet.Model
 {
     public class Person
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
---
+++ RestWithAspNet-02-UsingDifferentVerbs/RestWithAspNet/Services/Impl/PersonService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Services.Impl
+{
+    public class PersonService : IPersonService
+    {
+        //volatile garante que toda vez que a aplicacao rodar o count vai ser reinicializado
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person p = MockPerson(i);
+                persons.Add(p);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            Person p = new Person()
+            {
+                Id = IncrementAndGet(),
+                LastName = "Last"+i,
+                FirstName = "First" + i,
+                Address = "Rua x, campinas",
+                Gender = "Male"
+            };
+            return p;
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return MockPerson((int)id);
+        }
+
+        public Person Update(Person person)
+        {
+           return person;
+        }
+    }
+}
